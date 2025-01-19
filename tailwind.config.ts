b721882bcad9c9b924cf3e12eaf87eda7e4d0e98
Kaveesha <kaveeshewaduge111@gmail.com>
2025-01-19 21:23:59 +0530

    Set the file structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,10 @@ export default {
   				DEFAULT: 'hsl(var(--destructive))',
   				foreground: 'hsl(var(--destructive-foreground))'
   			},
+			dark: {
+				1: '#1C1F2E',
+				2: '#161925',
+			},
   			border: 'hsl(var(--border))',
   			input: 'hsl(var(--input))',
   			ring: 'hsl(var(--ring))',
